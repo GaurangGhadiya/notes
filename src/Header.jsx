9c472939283839c816c32dca06636dc62a9fd29c
Gaurang Ghadiya <gaurangghadiya99@gmail.com>
2020-10-01 16:55:35 +0530

    notes

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './Images/logo.png';
+
+const Header = () => {
+    return(
+        <>
+            <div className='header'>
+                <img src={logo} alt='logo' height='60' width='70'/>&nbsp;&nbsp;&nbsp;
+                <h1> Notes</h1>
+            </div>
+        </>
+    );
+}
+
+export default Header;
