8ab8a36df47cd1f3fdca9c2640aeabeb78f46719
Gaurang Ghadiya <gaurangghadiya99@gmail.com>
2020-10-01 17:10:09 +0530

    Updates

---
+++ precache-manifest.a090faf44f2014f8f80ceede3de7a2d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83986fbdcfd841ebf047fbb3ade00d6a",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "1b30317f4cf422799dbd",
+    "url": "/notes/static/css/main.3ce9fe9f.chunk.css"
+  },
+  {
+    "revision": "361e83a6aaf397202e36",
+    "url": "/notes/static/js/2.cf4f2b32.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/notes/static/js/2.cf4f2b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b30317f4cf422799dbd",
+    "url": "/notes/static/js/main.f86139b0.chunk.js"
+  },
+  {
+    "revision": "b4e7f333cec4847613e3",
+    "url": "/notes/static/js/runtime-main.9f0c36eb.js"
+  },
+  {
+    "revision": "59dd9160347dc4dd48c115d05b850b4d",
+    "url": "/notes/static/media/logo.59dd9160.png"
+  }
+]);
