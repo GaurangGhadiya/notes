3ab884fbe62091853b4417e09fe8dcc3b05893bc
Gaurang Ghadiya <gaurangghadiya99@gmail.com>
2020-10-02 13:10:01 +0530

    notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,44 @@
-import React, { useState } from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import CreateNote from './CreateNote';
-import Note from './Note';
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import CreateNote from "./CreateNote";
+import Note from "./Note";
 
 const App = () => {
+  const [addItem, setAddItem] = useState([]);
 
-    const [addItem, setAddItem] = useState([]);
+  const addNote = (note) => {
+    setAddItem((prevData) => {
+      return [...prevData, note];
+    });
+  };
 
-    const addNote = (note) => {
-        setAddItem((prevData) => {
-            return(
-                [...prevData, note]
-            );
-        })
-    }
-
-    const onDelete = (id) => {
-        setAddItem((olddata) =>
-            olddata.filter((currdata, indx) => {
-                return indx !== id; 
-            })
-        );
-    };
-
-    return(
-        <>
-            <Header />
-            <CreateNote passNote={addNote} />
-
-            {addItem.map ((val, index) => {
-                return (<Note key={index}
-                id={index}
-                title={val.title}
-                contant={val.contant}
-                deleteItem={onDelete}
-                />
-            );
-            })}
-
-            {/* <Footer /> */}
-        </>
+  const onDelete = (id) => {
+    setAddItem((olddata) =>
+      olddata.filter((currdata, indx) => {
+        return indx !== id;
+      })
     );
+  };
+
+  return (
+    <>
+      <Header />
+      <CreateNote passNote={addNote} />
+
+      {addItem.map((val, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={val.title}
+            contant={val.contant}
+            deleteItem={onDelete}
+          />
+        );
+      })}
+    </>
+  );
 };
 
 export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import logo from './Images/logo.png';
+import React from "react";
+import logo from "./Images/logo.png";
 
 const Header = () => {
-    return(
-        <>
-            <div className='header'>
-                <img src={logo} alt='logo' height='60' width='70'/>&nbsp;&nbsp;&nbsp;
-                <h1> Notes</h1>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="header">
+        <img src={logo} alt="logo" height="60" width="70" />
+        &nbsp;&nbsp;&nbsp;
+        <h1> Notes</h1>
+      </div>
+    </>
+  );
+};
 
 export default Header;
